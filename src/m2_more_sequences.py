566fd889cf9c6cd1fbfc72c087df2020a0b1a1d8
yans2 <yans2@rose-hulman.edu>
2019-01-20 14:51:50 -0500

    done

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -16,8 +16,8 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_shortest_string()
-    # run_test_index_of_largest_number()
+    # run_test_shortest_string()
+    run_test_index_of_largest_number()
     # run_test_number_of_stutters()
     # run_test_is_palindrome()
     # run_test_count_same()
@@ -103,7 +103,7 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
     least = strings[0]
@@ -184,10 +184,14 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
-
+    index_of_largest = 0
+    for k in range(n):
+        if numbers[k] > numbers[index_of_largest]:
+            index_of_largest = k
+    return index_of_largest
 
 # ----------------------------------------------------------------------
 # Some problems iterate (loop) through the sequence accessing TWO
@@ -242,7 +246,8 @@ def number_of_stutters(s):
     # TODO: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
-
+    count=0
+    for k in range
 
 def run_test_is_palindrome():
     """ Tests the   is_palindrome   function. """
